4749741aaf7426c689666b59eedebb513a0bcd36
hwarimryu <fwarim@gmail.com>
2024-11-18 22:47:17 +0900

    .

--- section13/src/App.jsx
+++ section13/src/App.jsx
@@ -3,15 +3,20 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Diary from './pages/Diary'
 import New from './pages/New'
+import NotFound from './pages/NotFound'
 
 function App() {
 
   return (
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='/new' element={<New />}/>
-      <Route path='/diary' element={<Diary />}/>
-    </Routes>
+    <>
+      <div>공통요소</div>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/new' element={<New />}/>
+        <Route path='/diary' element={<Diary />}/>
+        <Route path='*' element={<NotFound />}/>
+      </Routes>
+    </>
   )
 }
 
